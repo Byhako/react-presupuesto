e5c63664a4c5ae0204791eb6894297e558b26b67
byhako <eldragondera1@gmail.com>
2022-09-28 18:02:19 -0500

    Formulario

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+import Message from './Message'
 
 
-const NewBudget = () => {
+const NewBudget = ({ budget, setBudget }) => {
+  const [message, setMessage] = useState('')
+
+  const handleBudget = e => {
+    e.preventDefault()
+    
+    if (Number(budget) && Number(budget) > 0) {
+      setMessage('')
+
+    } else {
+      setMessage('No es un presupuesto válido.')
+    }
+  }
 
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-      <form className='formulario'>
+      <form onSubmit={handleBudget} className='formulario'>
         <div className="campo">
           <label htmlFor="nuevo-presupuesto">Definir Presupuesto</label>
           <input
@@ -12,10 +26,14 @@ const NewBudget = () => {
             className='nuevo-presupuesto'
             placeholder='Agrega tu presupuesto'
             id='nuevo-presupuesto'
+            value={budget}
+            onChange={e => setBudget(e.target.value)}
           />
         </div>
 
         <input type="submit" value='Agregar' />
+
+        {message && (<Message type='error'>{message}</Message>)}
       </form>
     </div>
   )
