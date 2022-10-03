b4a6071fe48e0e870ed7b158947c60ea94e7655f
byhako <eldragondera1@gmail.com>
2022-10-03 18:54:21 -0500

    Grafica de gastos

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const ControlBudget = ({ budget, setBudget, spents }) => {
+
+const ControlBudget = ({ budget, spents }) => {
 
   const formatNumber = n => n.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
 
   const [disponible, setdisponible] = useState(budget)
   const [gastado, setGastado] = useState(0)
+  const [percentage, setPercentage] = useState(0)
 
   useEffect(() => {
     const totalGastado = spents.reduce((total, gasto) => total + gasto.cantidad, 0)
     setGastado(totalGastado)
+    const per = 100/budget * totalGastado
+    setPercentage(per.toFixed(2))
     setdisponible(budget - totalGastado)
   }, [spents])
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        grafica aqui
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#3b82f6',
+            trailColor: '#777'
+          })}
+          value={percentage}
+          text={`${percentage}% gastado`}
+        />
       </div>
 
       <div className="contenido-presupuesto">
